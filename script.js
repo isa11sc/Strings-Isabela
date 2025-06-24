a81d3f35d27e0585f52cb3812f834a2c13346c95
isa11sc <isabela.ciscon@escola.pr.gov.br>
2025-06-24 14:28:16 +0000

    aula 4

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
+
     const texto = document.querySelector("#entrada-de-texto").value;
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
-   const palavras = texto.split(" ");
+   const palavraschave = processaTexto(texto);
 
-  campoResultado.textContent = palavras.join(", ");
+  campoResultado.textContent = palavraschave .join(", ");
+}
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
 }
